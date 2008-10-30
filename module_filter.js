c63dd8e0229b166ba2a4e088d12f4bfeb3ea8ab3
James Jeffery <jaysquare@gmail.com>
2008-10-30 17:24:25 +0000

    Committing new module that allows filtering of the module list.

---
+++ module_filter.js
@@ -0,0 +1,47 @@
+// $Id$
+
+if (Drupal.jsEnabled) {
+  var timeOut;
+  var filterText = '';
+
+  $(document).ready(function() {
+    $("#edit-module-filter").keyup(function() {
+      if (filterText != $(this).val()) {
+        if (timeOut) {
+          clearTimeout(timeOut);
+        }
+        filterText = $(this).val();
+        timeOut = setTimeout("moduleFilter('" + filterText + "')", 500);
+      }
+    });
+  });
+}
+
+function moduleFilter(filter) {
+  filterLowerCase = filter.toLowerCase();
+
+  $("table.package tbody tr td strong label").each(function(i) {
+    var parent = $(this).parent().parent().parent();
+    var module = $(this).text();
+
+    moduleLowerCase = module.toLowerCase();
+
+    if (moduleLowerCase.match(filterLowerCase)) {
+      if (parent.css('display') == 'none') {
+        parent.show();
+        if (parent.parent().parent().parent().parent().css('display') == 'none') {
+          parent.parent().parent().parent().parent().show();
+        }
+      }
+    } else {
+      if (parent.css('display') != 'none') {
+        parent.hide();
+        if (parent.siblings(":visible").html() == null) {
+          if (parent.parent().parent().parent().parent().css('display') != 'none') {
+            parent.parent().parent().parent().parent().hide();
+          }
+        }
+      }
+    }
+  });
+}
