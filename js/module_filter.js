c1fecfb1502798963db839754452b7c3d68d4184
James Jeffery <jaysquare@gmail.com>
2010-04-08 17:43:46 +0000

    #535736 by eMPee584, greenSkin: Added enabled, disabled, required, and unavailable options to filter by to the non-tabbed version.

--- js/module_filter.js
+++ js/module_filter.js
@@ -16,6 +16,19 @@ if (Drupal.jsEnabled) {
         moduleFilterTimeOut = setTimeout("moduleFilter('" + moduleFilterTextFilter + "')", 500);
       }
     });
+
+    $('#edit-module-filter-show-enabled').change(function() {
+      moduleFilter($('#edit-module-filter-name').val());
+    });
+    $('#edit-module-filter-show-disabled').change(function() {
+      moduleFilter($('#edit-module-filter-name').val());
+    });
+    $('#edit-module-filter-show-required').change(function() {
+      moduleFilter($('#edit-module-filter-name').val());
+    });
+    $('#edit-module-filter-show-unavailable').change(function() {
+      moduleFilter($('#edit-module-filter-name').val());
+    });
   });
 }
 
@@ -28,24 +41,62 @@ function moduleFilter(string) {
     var moduleLowerCase = module.toLowerCase();
     var $fieldset = $row.parents('fieldset');
 
+    if (string != '') {
+      if ($fieldset.hasClass('collapsed')) {
+        $fieldset.removeClass('collapsed');
+      }
+    }
+
     if (moduleLowerCase.match(stringLowerCase)) {
-      if (!$row.is(':visible')) {
-        $row.show();
-        if ($fieldset.hasClass('collapsed')) {
-          $fieldset.removeClass('collapsed');
+      if (moduleFilterVisible($('td.checkbox input', $row))) {
+        if (!$row.is(':visible')) {
+          $row.show();
+          if ($fieldset.hasClass('collapsed')) {
+            $fieldset.removeClass('collapsed');
+          }
+          if (!$fieldset.is(':visible')) {
+            $fieldset.show();
+          }
         }
-        if (!$fieldset.is(':visible')) {
-          $fieldset.show();
+      }
+      else {
+        $row.hide();
+        if ($row.siblings(':visible').html() == null) {
+          $fieldset.hide();
         }
       }
     }
     else {
-      if ($row.css('display') != 'none') {
+      if ($row.is(':visible')) {
         $row.hide();
-        if ($row.siblings(":visible").html() == null) {
+        if ($row.siblings(':visible').html() == null) {
           $fieldset.hide();
         }
       }
     }
   });
+}
+
+function moduleFilterVisible(checkbox) {
+  if ($('#edit-module-filter-show-enabled').is(':checked')) {
+    if ($(checkbox).is(':checked') && !$(checkbox).is(':disabled')) {
+      return true;
+    }
+  }
+  if ($('#edit-module-filter-show-disabled').is(':checked')) {
+    if (!$(checkbox).is(':checked') && !$(checkbox).is(':disabled')) {
+      return true;
+    }
+  }
+  if ($('#edit-module-filter-show-required').is(':checked')) {
+    if ($(checkbox).is(':checked') && $(checkbox).is(':disabled')) {
+      return true;
+    }
+  }
+  if ($('#edit-module-filter-show-unavailable').is(':checked')) {
+    if (!$(checkbox).is(':checked') && $(checkbox).is(':disabled')) {
+      return true;
+    }
+  }
+  return false;
 }
