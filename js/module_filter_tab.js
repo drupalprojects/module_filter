d49fda85b1c3d3ddcbb50a050ae65a1ffa5f89be
James Jeffery <jaysquare@gmail.com>
2010-10-22 23:32:17 +0000

    Added number of enabled of total number of modules to the active tab.

--- js/module_filter_tab.js
+++ js/module_filter_tab.js
@@ -131,6 +131,7 @@ Drupal.ModuleFilter.Tab = function(element) {
 Drupal.ModuleFilter.Tab.prototype.setActive = function() {
   if (Drupal.ModuleFilter.activeTab) {
     $(Drupal.ModuleFilter.activeTab.element).parent().removeClass('active');
+    $('span', Drupal.ModuleFilter.activeTab.element).empty();
   }
   // Assume the default active tab is #all-tab. Remove its active class.
   else {
@@ -169,4 +170,8 @@ Drupal.ModuleFilter.Tab.prototype.displayRows = function() {
       }
     });
   }
+
+  var enabled = $('#projects tbody tr:visible td.checkbox input:checked').length;
+  var total = $('#projects tbody tr:visible').length;
+  $('span', Drupal.ModuleFilter.activeTab.element).empty().append(Drupal.t('@enabled of @total', { '@enabled': enabled, '@total': total }));
 }
