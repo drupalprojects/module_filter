2824db7ec69a40beea310c075df5ef0d533032cf
James Jeffery <jaysquare@gmail.com>
2010-10-23 03:04:58 +0000

    Added some JavaScript code to help position the 'Save configuration' button for instances with lots of tabs.

--- js/module_filter_tab.js
+++ js/module_filter_tab.js
@@ -60,6 +60,24 @@ Drupal.behaviors.moduleFilter = function() {
   else if (Drupal.ModuleFilter.activeTab == undefined) {
     Drupal.ModuleFilter.activeTab = Drupal.ModuleFilter.tabs['all-tab'];
   }
+
+  $(window).scroll(function() {
+    var top = $('#module-filter-tabs').offset().top;
+    var bottom = top + $('#module-filter-tabs').height();
+    var windowHeight = $(window).height();
+    if (((bottom - windowHeight) > ($(window).scrollTop() - $('#module-filter-submit').height())) && $(window).scrollTop() + windowHeight - $('#module-filter-submit').height() - $('#all-tab').height() > top) {
+      if (!$('#module-filter-submit').hasClass('fixed')) {
+        $('#module-filter-submit').addClass('fixed');
+      }
+    }
+    else {
+      $('#module-filter-submit').removeClass('fixed');
+    }
+  });
+  $(window).trigger('scroll');
+  $(window).resize(function() {
+    $(window).trigger('scroll');
+  });
 }
 
 Drupal.ModuleFilter.visible = function(checkbox) {
