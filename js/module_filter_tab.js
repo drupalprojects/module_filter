65f560ac2eee8fed23431866210132d065e60b64
James Jeffery <jaysquare@gmail.com>
2011-09-14 12:21:13 -0700

    Implemented visual aids (displays number of modules being enabled/disabled as well as coloring the modules row accordingly).

--- js/module_filter_tab.js
+++ js/module_filter_tab.js
@@ -8,6 +8,10 @@ moduleGetID = function(text) {
   return id;
 };
 
+Drupal.ModuleFilter.tabs = {};
+Drupal.ModuleFilter.enabling = {};
+Drupal.ModuleFilter.disabling = {};
+
 Drupal.behaviors.moduleFilterTabs = {
   attach: function(context) {
     if (Drupal.settings.moduleFilter.tabs) {
@@ -32,14 +36,20 @@ Drupal.behaviors.moduleFilterTabs = {
         tabs += '</ul>';
         $('#module-filter-modules').before(tabs);
 
-        // Click event for tabs.
-        $('#module-filter-tabs li a').click(function() {
-          if ($(this).parent().hasClass('selected')) {
-            // Clear the active tab.
-            window.location.hash = '';
-            return false;
-          }
+        // Index tabs.
+        $('#module-filter-tabs li').each(function() {
+          var $tab = $(this);
+          var id = $tab.attr('id');
+          Drupal.ModuleFilter.tabs[id] = new Drupal.ModuleFilter.Tab($tab, id);
         });
+
+        if (Drupal.settings.moduleFilter.visualAid) {
+          $('#module-filter-modules tbody td.checkbox input').change(function() {
+            var $checkbox = $(this);
+            var type = ($checkbox.is(':checked')) ? 'enable' : 'disable';
+            Drupal.ModuleFilter.updateVisualAid(type, $checkbox.parents('tr'));
+          });
+        }
 
         // Add filter rule to limit by active tab.
         moduleFilter.options.rules.push(function(moduleFilter, item) {
@@ -77,6 +87,21 @@ Drupal.behaviors.moduleFilterTabs = {
   }
 }
 
+Drupal.ModuleFilter.Tab = function(element, id) {
+  var self = this;
+
+  this.id = id.substring(0, id.length - 4);
+  this.element = element;
+
+  $('a', this.element).click(function() {
+    if (self.element.hasClass('selected')) {
+      // Clear the active tab.
+      window.location.hash = '';
+      return false;
+    }
+  });
+}
+
 Drupal.ModuleFilter.eventHandlerOperateByURLFragment = function(event) {
   if (Drupal.ModuleFilter.activeTab != undefined) {
     Drupal.ModuleFilter.activeTab.element.removeClass('selected');
@@ -100,6 +125,87 @@ Drupal.ModuleFilter.eventHandlerOperateByURLFragment = function(event) {
 
 Drupal.ModuleFilter.countSummary = function(id) {
   return Drupal.t('@enabled of @total', { '@enabled': Drupal.settings.moduleFilter.enabledCounts[id].enabled, '@total': Drupal.settings.moduleFilter.enabledCounts[id].total });
+}
+
+Drupal.ModuleFilter.Tab.prototype.updateEnabling = function(amount) {
+  this.enabling = this.enabling || 0;
+  this.enabling += amount;
+  if (this.enabling == 0) {
+    delete this.enabling;
+  }
+}
+
+Drupal.ModuleFilter.Tab.prototype.updateDisabling = function(amount) {
+  this.disabling = this.disabling || 0;
+  this.disabling += amount;
+  if (this.disabling == 0) {
+    delete this.disabling;
+  }
+}
+
+Drupal.ModuleFilter.Tab.prototype.updateVisualAid = function() {
+  var visualAid = '';
+  if (this.enabling != undefined) {
+    visualAid += '<span class="enabling">' + Drupal.t('+@count', { '@count': this.enabling });
+  }
+  if (this.disabling != undefined) {
+    visualAid += '<span class="disabling">' + Drupal.t('-@count', { '@count': this.disabling });
+  }
+
+  if (this.visualAid == undefined) {
+    $('a', this.element).prepend('<span class="visual-aid"></span>');
+    this.visualAid = $('span.visual-aid', this.element);
+  }
+
+  this.visualAid.empty().append(visualAid);
+}
+
+Drupal.ModuleFilter.updateVisualAid = function(type, $row) {
+  var id = $row.data('moduleFilterTabID');
+  if (!id) {
+    // Find the tab ID.
+    var classes = $row.attr('class').split(' ');
+    for (var i in classes) {
+      if (Drupal.ModuleFilter.tabs[classes[i]] != undefined) {
+        id = classes[i];
+      }
+    }
+  }
+
+  if (!id) {
+    return false;
+  }
+
+  var tab = Drupal.ModuleFilter.tabs[id];
+  var name = $('td:nth(1)', $row).text();
+  switch (type) {
+    case 'enable':
+      if (Drupal.ModuleFilter.disabling[id + name] != undefined) {
+        delete Drupal.ModuleFilter.disabling[id + name];
+        tab.updateDisabling(-1);
+        $row.removeClass('disabling');
+      }
+      else {
+        Drupal.ModuleFilter.enabling[id + name] = name;
+        tab.updateEnabling(1);
+        $row.addClass('enabling');
+      }
+      break;
+    case 'disable':
+      if (Drupal.ModuleFilter.enabling[id + name] != undefined) {
+        delete Drupal.ModuleFilter.enabling[id + name];
+        tab.updateEnabling(-1);
+        $row.removeClass('enabling');
+      }
+      else {
+        Drupal.ModuleFilter.disabling[id + name] = name;
+        tab.updateDisabling(1);
+        $row.addClass('disabling');
+      }
+      break;
+  }
+
+  tab.updateVisualAid();
 }
 
 })(jQuery);
