69e7715f93f35b2f916c127b3d54bca36e6cd7e9
James Jeffery <jaysquare@gmail.com>
2012-01-30 16:22:04 -0800

    by greenSkin: Implemented a "switch" look instead of checkboxes.

--- js/module_filter_tab.js
+++ js/module_filter_tab.js
@@ -179,6 +179,15 @@ Drupal.behaviors.moduleFilterTabs = {
         else {
           Drupal.ModuleFilter.selectTab('all');
         }
+
+        $('td.checkbox div.form-item').hide();
+        $('td.checkbox').each(function(i) {
+          var $cell = $(this);
+          $('.toggle-enable', $cell).removeClass('js-hide').click(function() {
+            $(this).toggleClass('off');
+            $('div.form-item input', $cell).change().click();
+          });
+        });
       });
     }
   }
